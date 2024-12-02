a7aa01fab35a9ea4b793a68192bba4ea01fa9d31
sushichan044 <mail@sushichan.live>
2024-12-02 14:47:13 +0900

    fix: split ghq conf

---
+++ dot_zsh.d/ghq.zsh
@@ -0,0 +1,20 @@
+function peco-ghq() {
+    if ! type ghq >/dev/null 2>&1; then
+        echo "ghq is not installed"
+        return 1
+    fi
+    if ! type peco >/dev/null 2>&1; then
+        echo "peco is not installed"
+        return 1
+    fi
+
+    local selected_dir=$(ghq list -p | peco --prompt="repositories >" --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-ghq
+bindkey '^]' peco-ghq
+alias gz='peco-ghq'
