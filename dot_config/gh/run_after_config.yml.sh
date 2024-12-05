1f55bf5dd8cc90371945453227a1e1264f898d1e
sushichan044 <mail@sushichan.live>
2024-12-05 22:46:42 +0900

    gh auth を先に確認する

--- dot_config/gh/run_after_config.yml.sh
+++ dot_config/gh/run_after_config.yml.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+if ! type gh >/dev/null 2>&1; then
+    echo "❌ gh is not available. Exiting..."
+    exit 1
+fi
+
+if ! gh auth status >/dev/null 2>&1; then
+    echo "❌ gh is not authenticated. Exiting..."
+    exit 1
+fi
+
 gh extension upgrade --all
