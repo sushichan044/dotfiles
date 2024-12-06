332258e0cbaa7d94bb21861e10e18765bfcd3928
sushichan044 <mail@sushichan.live>
2024-12-07 04:47:52 +0900

    format tab title

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ config.use_ime = true
 config.window_decorations = "RESIZE"
 
 -- Appearance
-function scheme_for_appearance(appearance)
+local function scheme_for_appearance(appearance)
     if appearance:find "Dark" then
         return "Catppuccin Macchiato"
     else
@@ -34,11 +34,10 @@ config.font = wezterm.font 'MesloLGS NF'
 config.font_size = 13.0
 
 -- Tabs
-config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.show_close_tab_button_in_tabs = false
 config.tab_and_split_indices_are_zero_based = true
-config.tab_max_width = 6
+config.tab_max_width = 18
 config.colors = {
     tab_bar = {
         inactive_tab_edge = "none",
@@ -73,6 +72,49 @@ config.keys = {
         action = wezterm.action.ActivatePaneDirection "Next"
     },
 }
+
+-- Return the Tab's current working directory
+local function get_cwd(tab)
+    return tab.active_pane.current_working_dir.file_path or ""
+end
+
+-- Remove all path components and return only the last value
+local function remove_abs_path(path) return path:gsub("(.*[/\\])(.*)", "%2") end
+
+-- Return the pretty path of the tab's current working directory
+local function get_display_cwd(tab)
+    local current_dir = get_cwd(tab):gsub("/$", "") -- Remove trailing slash
+    local HOME_DIR = os.getenv("HOME")
+
+    return current_dir == HOME_DIR and "~" or remove_abs_path(current_dir) .. '/'
+end
+
+-- Pretty format the tab title
+local function format_title(tab, max_width)
+    local pretty_cwd = get_display_cwd(tab)
+
+    if #pretty_cwd > max_width then
+        pretty_cwd = wezterm.truncate_right(pretty_cwd, max_width - 3) .. '../'
+    end
+
+    return string.format("%s", pretty_cwd)
+end
+
+wezterm.on(
+    'format-tab-title',
+    function(tab, tabs, panes, config, hover, max_width)
+        local minimum_title_length = 4 + #tostring(tab.tab_index)
+        local title = string.format(
+            ' %s: %s ',
+            tab.tab_index,
+            format_title(tab, max_width - minimum_title_length)
+        )
+
+        return {
+            { Text = title }
+        }
+    end
+)
 
 -- and finally, return the configuration to wezterm
 return config
