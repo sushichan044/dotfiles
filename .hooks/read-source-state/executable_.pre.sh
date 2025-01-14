1d92884a07ebca419f0cd6b0cdb5e3aa77b32e9e
sushichan044 <mail@sushichan.live>
2025-01-14 16:34:50 +0900

    fix: change hook script

--- .hooks/read-source-state/executable_.pre.sh
+++ .hooks/read-source-state/executable_.pre.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
+### Check execution environment
+if [ "${CHEZMOI:-0}" -ne 1 ]; then
+    echo "❌ This script should only be run by chezmoi."
+    exit 1
+fi
+
+### Check OS
+is_macos() {
+    [ "$(uname -s)" = "Darwin" ]
+}
+
+is_linux() {
+    [ "$(uname -s)" = "Linux" ]
+}
+
+if ! is_macos && ! is_linux; then
+    echo "❌ sushichan044/dotfiles only supports macOS and Linux."
+    exit 1
+fi
+
 # exit immediately if password-manager-binary is already in $PATH
 type op >/dev/null 2>&1 && exit
 
 CHEZMOI_BIN_DIR="$HOME/.chezmoi-bin"
 mkdir -p "$CHEZMOI_BIN_DIR"
 
-case "$(uname -s)" in
-Darwin)
+if is_macos; then
     # commands to install password-manager-binary on Darwin
     mkdir -p ~/.dotfiles/.tmp
     curl -L https://cache.agilebits.com/dist/1P/op2/pkg/v2.30.0/op_darwin_arm64_v2.30.0.zip -o ~/.dotfiles/.tmp/op.zip
@@ -15,8 +34,7 @@ Darwin)
     mv ~/.dotfiles/.tmp/op "$CHEZMOI_BIN_DIR"
     # shellcheck disable=SC1091
     export PATH="$CHEZMOI_BIN_DIR:$PATH"
-    ;;
-Linux)
+elif is_linux; then
     # commands to install password-manager-binary on Linux
     curl -sS https://downloads.1password.com/linux/keys/1password.asc |
         sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
@@ -29,9 +47,4 @@ Linux)
     curl -sS https://downloads.1password.com/linux/keys/1password.asc |
         sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
     sudo apt update && sudo apt install 1password-cli
-    ;;
-*)
-    echo "unsupported OS"
-    exit 1
-    ;;
-esac
+fi
