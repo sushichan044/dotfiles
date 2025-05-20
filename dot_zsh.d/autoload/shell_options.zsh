82078b5f4880342f0fda7e87971861da27abfc47
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-05-20 23:11:13 +0900

    chore: remove slash from WORDCHARS

--- dot_zsh.d/autoload/shell_options.zsh
+++ dot_zsh.d/autoload/shell_options.zsh
@@ -12,3 +12,7 @@ setopt hist_ignore_space
 setopt share_history
 SAVEHIST=100
 HISTFILE=$HOME/.zsh_history
+
+# '/' を単語の境界として認める
+# https://blog.3qe.us/entry/2025/05/20/201219
+typeset -g WORDCHARS=${WORDCHARS:s@/@}
