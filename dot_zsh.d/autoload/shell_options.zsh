d406517b0610439d2903635ac001ac6027b9c040
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-07-03 03:00:40 +0900

    iikanji

--- dot_zsh.d/autoload/shell_options.zsh
+++ dot_zsh.d/autoload/shell_options.zsh
@@ -6,9 +6,12 @@ colors
 setopt no_beep
 setopt globdots
 setopt auto_cd
+setopt interactive_comments
 
 setopt hist_ignore_dups
+setopt hist_ignore_all_dups
 setopt hist_ignore_space
+setopt hist_reduce_blanks
 setopt share_history
 SAVEHIST=100
 HISTFILE=$HOME/.zsh_history
