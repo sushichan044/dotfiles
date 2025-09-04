d69fd52e9a2f6ce9ab78bb0bd161017cda2208b9
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-09-04 18:59:51 +0900

    graceful exit

--- .chezmoiscripts/run_rust_update.sh
+++ .chezmoiscripts/run_rust_update.sh
@@ -3,6 +3,6 @@ if type "rustup" >/dev/null 2>&1; then
     rustup update
     echo "✅ Rust toolchain updated successfully."
 else
-    echo "❌ Command 'rustup' not found. Exiting..."
-    exit 1
+    echo "⚠️ Command 'rustup' not found. Exiting..."
+    exit 0
 fi
