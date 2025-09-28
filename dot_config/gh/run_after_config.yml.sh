80bd256d1d261d0dff120a00f8be55c5b607e2a4
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-09-28 15:59:52 +0900

    gracefully shutdown

--- dot_config/gh/run_after_config.yml.sh
+++ dot_config/gh/run_after_config.yml.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if ! type gh >/dev/null 2>&1; then
-    echo "❌ gh is not available. Exiting..."
-    exit 1
+    echo "⚠️ gh is not available. Exiting..."
+    exit 0
 fi
 
 if ! gh auth status >/dev/null 2>&1; then
-    echo "❌ gh is not authenticated. Exiting..."
-    exit 1
+    echo "⚠️ gh is not authenticated. Exiting..."
+    exit 0
 fi
 
 gh extension upgrade --all
