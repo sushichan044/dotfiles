acb63ff32ccbbcafe08f2195e227ae7d2f5cc26a
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-09-28 20:04:04 +0900

    inject credential on update command only

--- dot_zsh.d/autoload/chezmoi.zsh
+++ dot_zsh.d/autoload/chezmoi.zsh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-if command_exists op; then
-    cz() {
-        local env_files=(
-            "$HOME/.sushichan044/op/chezmoi.env"
-            "$HOME/.sushichan044/op/mise.env"
-        )
-
-        local args=()
-        for f in "${env_files[@]}"; do
-            args+=(--env-file="$f")
-        done
-
-        op run "${args[@]}" -- chezmoi "$@"
-    }
-else
-    cz() {
-        echo "⚠️ op is not available or env files are missing. Running chezmoi without op."
-        chezmoi "$@"
-    }
-fi
-
 update() {
-    cz update
-    cz apply
+    chezmoi update
+
+    local env_files=(
+        "$HOME/.sushichan044/op/chezmoi.env"
+        "$HOME/.sushichan044/op/mise.env"
+    )
+
+    local args=()
+    for f in "${env_files[@]}"; do
+        args+=(--env-file="$f")
+    done
+
+    op run "${args[@]}" -- chezmoi apply
 }
