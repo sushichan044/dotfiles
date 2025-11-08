b9fa2be94ac4355d89aa5866055f115c485be713
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-09 01:29:07 +0900

    chore

--- dot_zsh.d/autoload/shell_options.zsh
+++ dot_zsh.d/autoload/shell_options.zsh
@@ -1,20 +1,26 @@
 typeset -gU path PATH
 
-autoload -U colors
+autoload -Uz colors
 colors
+
+setopt ignoreeof
+
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
 
 setopt no_beep
 setopt globdots
-setopt auto_cd
 setopt interactive_comments
 
-setopt hist_ignore_dups
+setopt share_history
 setopt hist_ignore_all_dups
 setopt hist_ignore_space
 setopt hist_reduce_blanks
-setopt share_history
-SAVEHIST=100
-HISTFILE=$HOME/.zsh_history
+
+HISTFILE=$ZDOTDIR/.zsh_history
+HISTSIZE=2000
+SAVEHIST=2000
 
 # '/' を単語の境界として認める
 # https://blog.3qe.us/entry/2025/05/20/201219
