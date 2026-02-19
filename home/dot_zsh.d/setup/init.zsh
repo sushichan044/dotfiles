e58fcc5ab158d1e8e465f25025eca08c01904735
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-02-19 09:34:47 +0900

    move shell options to init.zsh

--- home/dot_zsh.d/setup/init.zsh
+++ home/dot_zsh.d/setup/init.zsh
@@ -1,3 +1,32 @@
+typeset -gU path PATH
+
+autoload -Uz colors
+colors
+
+setopt ignoreeof
+setopt extended_glob
+
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+
+setopt no_beep
+setopt globdots
+setopt interactive_comments
+
+setopt share_history
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=2000
+SAVEHIST=2000
+
+# '/' を単語の境界として認める
+# https://blog.3qe.us/entry/2025/05/20/201219
+typeset -g WORDCHARS=${WORDCHARS:s@/@}
+
 command_exists() {
     type "$1" >/dev/null 2>&1
 }
