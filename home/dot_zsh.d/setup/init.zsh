dc53706fa1d1c5f40f6caac2ad5763f513a895d2
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-02-25 13:38:19 +0900

    fix broken path handling

--- home/dot_zsh.d/setup/init.zsh
+++ home/dot_zsh.d/setup/init.zsh
@@ -70,7 +70,7 @@ add_to_path_if_not_exists() {
     fi
 
     case ":$PATH:" in
-    *":?$dir:"*) ;; # 先頭に追加された場合 <dir>: となるため、最初の : はない場合がある
+    *":$dir:"*) ;;
     *) export PATH="$dir:$PATH" ;;
     esac
 }
@@ -82,7 +82,7 @@ add_to_manpath_if_not_exists() {
     fi
 
     case ":$MANPATH:" in
-    *":?$dir:"*) ;; # 先頭に追加された場合 <dir>: となるため、最初の : はない場合がある
+    *":$dir:"*) ;;
     *) export MANPATH="$dir:$MANPATH" ;;
     esac
 }
